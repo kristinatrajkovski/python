01028b9c4e578c543dbc94253e225e11e7055759
kristinatrajkovski <68972243+kristinatrajkovski@users.noreply.github.com>
2021-06-15 11:07:08 +0200

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+n=1
+while n<=1000:
+    if (n%3==0) and (n%5==0):
+        print("FizzBuzz")
+    elif (n%3==0):
+        print("Fizz")
+    elif (n%5==0):
+        print("Buzz")
+    else:
+        print(n)
+
+    n=n+1
+
