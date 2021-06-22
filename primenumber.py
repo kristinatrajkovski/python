df0a89508e8c0368c66fbe5e0c2c84c5995cf4c4
kristinatrajkovski <68972243+kristinatrajkovski@users.noreply.github.com>
2021-06-22 23:38:13 +0300

    Update primenumber.py

--- primenumber.py
+++ primenumber.py
@@ -1,12 +1,23 @@
 def is_prime(n):
-    if n>1:
-        if (n==2) or (n==3) or (n==5):
-            return True
-        elif (((n+1)%6==0) or ((n-1)%6==0)) and (n%5!=0):
+    t=2
+    l=[]
+    while (t<=n):
+        m=0
+        for i in l:
+            if t%i==0:
+                m=m+1
+                break
+
+        if m==0:
+            l.append(t)
+        
+        t=t+1
+    
+    if (n>1):
+        if (l[len(l)-1]==n):
             return True
         else:
             return False
-    else:
+    else: 
         return False
-
 
