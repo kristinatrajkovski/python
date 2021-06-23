8216bcf5c9b8dacaee716070ecf57e48b075783f
kristinatrajkovski <68972243+kristinatrajkovski@users.noreply.github.com>
2021-06-23 19:20:10 +0300

    Update palindrome_primes.py

--- palindrome_primes.py
+++ palindrome_primes.py
@@ -1,18 +1,22 @@
-l=[2]
-t=2
-p=[]
-while (t<100000):
-    m=0
-    for i in l:
-        if t%i==0:
-            m=m+1
-            break 
+import math
+def is_prime(n):
+    t=2
+    l=[]
+    while (t<=math.sqrt(n)):
+        if n%t==0:
+            return False
+            break
+        t=t+1
+        if (t>=math.sqrt(n))and(n%t!=0):
+            return True
 
-    if (m==0):
-        l.append(t)
-        if (t>10000) and (t//10000==t%10) and ((t//1000)%10==(t%100)//10):
-            p.append(t)
-    t=t+1
 
-print(p)
+i=10000
+l=[]
+while (i<100000):
+    if (i//10000==i%10) and ((i//1000)%10==(i%100)//10) and is_prime(i):
+        l.append(i)
+    i=i+1
+
+print(l)
 
