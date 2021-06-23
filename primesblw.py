41d4ddb0a90d2205d26df785f261758a6ad5f3d2
kristinatrajkovski <68972243+kristinatrajkovski@users.noreply.github.com>
2021-06-23 19:25:58 +0300

    Update primesblw.py

--- primesblw.py
+++ primesblw.py
@@ -1,16 +1,18 @@
-  
+import math
 def primes_below(n):
     t=2
     l=[]
-    while (t<=n):
+    while (t<n):
         m=0
         for i in l:
             if t%i==0:
                 m=m+1
+                break
+            if (i>math.sqrt(t)):
                 break
 
         if m==0:
             l.append(t)
         
         t=t+1
-return l
+    return l
